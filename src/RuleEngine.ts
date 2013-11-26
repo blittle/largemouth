6fb13218a92ece26f91d390f4c360530ec59d9f9
Bret Little <bret.little@gmail.com>
2013-11-26 16:37:07 -0500

    Add first round of a rule/permission engine and associated tests

---
+++ src/RuleEngine.ts
@@ -0,0 +1,77 @@
+///<reference path="../d.ts/DefinitelyTyped/underscore/underscore.d.ts"/>
+import _ = require('lodash');
+
+var RULE_KEYS = {
+	READ: ".read",
+	WRITE: ".write",
+	VALIDATE: ".validate"
+};
+
+class RuleEngine {
+
+	private config: any;
+
+	/**
+	 * Build a new RuleEngine which will parse and return permissions for
+	 * various paths inside the configuration. If a configuration is not found
+	 * for a path, the closest parent permissions will be used instead.
+	 * @param config
+	 */
+	constructor(config: any) {
+		if(typeof config == 'undefined' || config == null) {
+			config = {
+				".read": true,
+				".write": true
+			}
+		}
+		this.config = config;
+	}
+
+	private getRuleDefinition(path: string): any {
+		if(!path) return this.config;
+		if(path[0] === '/') path = path.substring(1);
+
+		var config = this.config;
+		var paths = path.split('/');
+
+		// Use _.find() so we can short circuit once
+		// we found the desired rule definition
+		_.find(paths, (path, i): any => {
+			if(!config[path]) return true;
+			config = config[paths];
+		});
+
+		return config;
+	}
+
+	/**
+	 * Given the permission configuration, can a value be written at the given path?
+	 *
+	 * @param path - a string representing the path to the data to change
+	 * @returns boolean
+	 */
+	canWrite(path: string): boolean {
+		return this.getRuleDefinition(path)[RULE_KEYS.WRITE];
+	}
+
+	/**
+	 * Given the permission configuration, can a value be read at the given path?
+	 * @param path - a string representing the path to the data to read
+	 * @returns boolean
+	 */
+	canRead(path: string): boolean {
+		return this.getRuleDefinition(path)[RULE_KEYS.READ];
+	}
+
+	/**
+	 * Given the permission configuration, is the value to be written valid?
+	 * @param path - a string representing the path to the data to change
+	 * @param any - the actual data being validated
+	 * @returns boolean
+	 */
+	validate(path: string, data: any): boolean {
+		return true;
+	}
+}
+
+export = RuleEngine;
---
+++ test/Rules.spec.js
@@ -0,0 +1,91 @@
+var RuleEngine = require('../src/RuleEngine');
+
+describe('In Memory DB', function () {
+	var ruleEngine;
+
+	beforeEach(function () {
+		ruleEngine = new RuleEngine();
+	});
+
+	afterEach(function() {
+	});
+
+	it('Should default to all rights', function() {
+		expect(ruleEngine.canWrite('some/random/path')).toBeTruthy();
+		expect(ruleEngine.canWrite('/some/other/random/path')).toBeTruthy();
+
+		expect(ruleEngine.canRead('some/random/path')).toBeTruthy();
+		expect(ruleEngine.canRead('/some/other/random/path')).toBeTruthy();
+	});
+
+	it('Should correctly resolve permissions for the root', function() {
+		ruleEngine = new RuleEngine({
+			".read": false,
+			".write": false
+		});
+
+		expect(ruleEngine.canRead('/')).toBeFalsy();
+		expect(ruleEngine.canRead('')).toBeFalsy();
+
+		expect(ruleEngine.canWrite('/')).toBeFalsy();
+		expect(ruleEngine.canWrite('')).toBeFalsy();
+	});
+
+	it('Should correctly resolve permissions for children that don\'t have defined permissions', function() {
+		ruleEngine = new RuleEngine({
+			".read": false,
+			".write": false
+		});
+
+		expect(ruleEngine.canRead('/my/path/down')).toBeFalsy();
+		expect(ruleEngine.canRead('my/path/down')).toBeFalsy();
+
+		expect(ruleEngine.canWrite('/way/truth/life')).toBeFalsy();
+		expect(ruleEngine.canWrite('way/truth/life')).toBeFalsy();
+	});
+
+	it('Should correctly resolve permissions for children 1', function() {
+		ruleEngine = new RuleEngine({
+			".read": false,
+			".write": false,
+			'chats': {
+				".read": true,
+				".write": false,
+				"name": {
+					".read": true,
+					".write": true
+				}
+			}
+		});
+
+		expect(ruleEngine.canRead('')).toBeFalsy();
+		expect(ruleEngine.canWrite('')).toBeFalsy();
+
+		expect(ruleEngine.canRead('chats')).toBeTruthy();
+		expect(ruleEngine.canWrite('chats')).toBeFalsy();
+
+		expect(ruleEngine.canRead('chats/name')).toBeTruthy();
+		expect(ruleEngine.canWrite('chats/name')).toBeTruthy();
+	});
+
+	it('Should correctly resolve permissions for children 2', function() {
+		ruleEngine = new RuleEngine({
+			'chats': {
+				".read": true,
+				"name": {
+					".read": true,
+					".write": true
+				}
+			}
+		});
+
+		expect(ruleEngine.canRead('')).toBeFalsy();
+		expect(ruleEngine.canWrite('')).toBeFalsy();
+
+		expect(ruleEngine.canRead('chats')).toBeTruthy();
+		expect(ruleEngine.canWrite('chats')).toBeFalsy();
+
+		expect(ruleEngine.canRead('chats/name')).toBeTruthy();
+		expect(ruleEngine.canWrite('chats/name')).toBeTruthy();
+	});
+});
