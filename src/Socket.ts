b4ddb57aef029a50fe79d74fd726bfe81f484150
Bret Little <bret.little@gmail.com>
2014-01-09 11:26:15 -0700

    Correctly modify the typescript :(

--- src/Socket.ts
+++ src/Socket.ts
@@ -18,52 +18,70 @@ var start = function(socketio: any, db: Database.db, config: Config.Interface, o
 
 	var tokens = {};
 
-//	io.configure(function() {
-//		io.set('authorization', function(handshakeData, callback) {
-//			if(config.auth) config.auth(handshakeData, callback);
-//			else {
-//				handshakeData.token = Math.ceil(Math.random() * 10000000);
-//				callback(null, true);
-//			}
-//		});
-//	})
-
-
 	io.sockets.on('connection', function (socket: Socket) {
+
+		var authenticated = false;
+
+		function authCheck() {
+			if(!authenticated) {
+				console.error('not authenticated');
+				socket.emit('error', 'not authenticated');
+			}
+		}
 
 		var handshake = socket.manager.handshaken[socket.id];
 
 		if(handshake) tokens[handshake.token] = socket.id;
 
-		socket.emit('ready', {
-			id: socket.id,
-			token: handshake ? handshake.token : null
+		socket.emit('ready');
+
+		socket.on('auth', function(req) {
+			if(config.auth) {
+				config.auth(req, function(error, auth, token) {
+					if(!error && auth) {
+						authenticated = true;
+						socket.emit('auth', {
+							id: socket.id,
+							token: token
+						});
+					} else {
+						socket.emit('error', 'auth error');
+						socket.disconnect();
+					}
+				});
+			}
 		});
 
 		socket.on('subscribe', function(req) {
+			if(authCheck()) return;
 			console.log('subscribe', req.path);
 			adapter.get(req, socket);
 		});
 
 		socket.on('set', function(req) {
+			if(authCheck()) return;
 			console.log('set', req.path, req.value);
 			adapter.set(req, socket); });
 
 		socket.on('update', function(req) {
+			if(authCheck()) return;
 			adapter.update(req, socket);
 		});
 
 		socket.on('remove', function(req) {
+			if(authCheck()) return;
 			adapter.remove(req, socket);
 		});
 
 		socket.on('disconnect', function() {
+			if(authCheck()) return;
 			adapter.clearSubscription(socket);
 		});
 
 		// Initialize custom events
 		_.each(config.events, function(event, key: string) {
 			socket.on(key, function(req) {
+				if(authCheck()) return;
 				event(req, socket, adapter);
 			});
 		});
